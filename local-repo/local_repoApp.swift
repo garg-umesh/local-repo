7e6ab474fbeb74658338d4dee3e83a8c09b075cd
Umesh Garg <umeshgarg3434@gmail.com>
2022-03-07 21:16:57 -0500

    Initial Commit

---
+++ local-repo/local_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_repoApp.swift
+//  local-repo
+//
+//  Created by Umesh Garg on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct local_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
